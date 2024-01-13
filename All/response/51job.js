b19c0ba909ba0256c7192b0f76ab82167625c8b7
Dark Wang <zirawell@gmail.com>
2024-01-13 23:29:07 +0800

    update 51job

--- All/response/51job.js
+++ All/response/51job.js
@@ -4,7 +4,6 @@ try {
   let result = obj.resultbody;
   result.adsTabFeeds=[];
   body = JSON.stringify(obj);
-  console.log("body"+body);
 } catch (error) {
   console.log(`51job获取错误: ` + error);
 }
