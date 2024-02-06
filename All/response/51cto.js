0202758877fd03c6ea57a76d3e1f188b3d9bb59b
Dark Wang <zirawell@gmail.com>
2024-02-06 13:13:07 +0800

    update

--- All/response/51cto.js
+++ All/response/51cto.js
@@ -3,6 +3,9 @@ try {
   let obj = JSON.parse(body);
   let result = obj.result;
   result.adv_summary={};
+  result.hasAd="0";
+  result.indexAd={};
+  result.ad=[];
   body = JSON.stringify(obj);
 } catch (error) {
   console.log(`51cto获取错误: ` + error);
