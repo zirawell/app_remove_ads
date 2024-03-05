a3d056594bbe757fd0329e621076d00cdd43ac6d
Dark Wang <zirawell@gmail.com>
2024-03-05 12:54:37 +0800

    update freshippo

--- All/response/freshippo.js
+++ All/response/freshippo.js
@@ -3,38 +3,28 @@ const isResponse = typeof $response !== "undefined";
 
 switch (isResponse) {
   case /^https?:\/\/acs\.m\.taobao\.com\/gw\/mtop\.wdk\.render\.queryindexpage/.test(url):
-      try {
-        let body = $response.body;
-        let obj = JSON.parse(body);
-        const removeSceneTemplateId = [
-          "358",
-        ];
-        if (obj?.data？.scenes?.length > 0) {
-          let scenes = [];
-          for (let scene of obj.data.scenes) {
-            if (!removeSceneTemplateId?.includes(scene.sceneTemplateId)) {
-              scenes.push(scene);
-            }
-          }
-          obj.data.scenes = scenes;
+  try {
+    let body = $response.body;
+    let obj = JSON.parse(body);
+    const sceneTemplateId = [
+      "509",
+      "738",
+      
+    ];
+    if (obj?.data?.scenes?.length > 0) {
+      let scenes = [];
+      for (let scene of obj.data.scenes) {
+        if (sceneTemplateId?.includes(scene?.sceneTemplateId)) {
+          scenes.push(scene);
         }
-        body = JSON.stringify(obj);
-      } catch (error) {
-        console.log(`盒马主页获取错误: ` + error);
       }
-  break;
-
-  case /^https?:\/\/acs\.m\.taobao\.com\/gw\/mtop\.wdk\.sg\.querysinglescene/.test(url):
-      try {
-        let body = $response.body;
-        let obj = JSON.parse(body);
-        if (obj?.data?.content !== "undefined") {
-          obj.data.content=[];
-        }
-        body = JSON.stringify(obj);
-      } catch (error) {
-        console.log(`盒马悬浮doodle获取错误: ` + error);
-      }
+      obj.data.scenes = scenes;
+    }
+    body = JSON.stringify(obj);
+    
+  } catch (error) {
+    console.log(`盒马主页获取错误: ` + error);
+  }
   break;
 
   default:
