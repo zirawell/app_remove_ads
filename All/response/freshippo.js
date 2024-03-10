644d05720bee5841a68fc0a76a4a16632f0a27fa
Dark Wang <zirawell@gmail.com>
2024-03-10 16:01:58 +0800

    update-20240310

--- All/response/freshippo.js
+++ All/response/freshippo.js
@@ -26,6 +26,40 @@ switch (isResponse) {
   }
   break;
 
+  case /^https:\/\/acs\.m\.taobao\.com\/gw\/mtop\.wdk\.render\.querymypage/.test(url):
+  try {
+    let body = $response.body;
+    let obj = JSON.parse(body);
+    const sceneTemplateId = [
+      "906",
+      "907",
+      "198",
+      "286",
+      "431",
+      "185",
+      "230",
+      "978",
+      "709",
+      "432",
+      "403",
+      "350",
+      
+    ];
+    if (obj?.data?.scenes?.length > 0) {
+      let scenes = [];
+      for (let scene of obj.data.scenes) {
+        if (sceneTemplateId?.includes(scene?.sceneTemplateId)) {
+          scenes.push(scene);
+        }
+      }
+      obj.data.scenes = scenes;
+    }
+    body = JSON.stringify(obj);
+  } catch (error) {
+    console.log(`盒马我的页获取错误: ` + error);
+  }
+  break;
+
   default:
   $done({});
 }
