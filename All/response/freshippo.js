cdf58d9964509d1805957fb682710dd0f5c73bdc
Dark Wang <zirawell@gmail.com>
2024-03-11 08:55:23 +0800

    update-freshippo

---
+++ All/response/freshippo.js
@@ -0,0 +1,54 @@
+const url = $request.url;
+let obj=JSON.parse($response.body);
+
+if(url.indexOf("queryindexpage")!=-1){
+  try {
+    const sceneTemplateId = [
+      "509",
+      "738"
+    ];
+    if (obj?.data?.scenes?.length > 0) {
+      let scenes = [];
+      for (let scene of obj.data.scenes) {
+        if (sceneTemplateId.includes(scene.sceneTemplateId)) {
+          scenes.push(scene);
+        }
+      }
+      obj.data.scenes = scenes;
+    }
+  } catch (error) {
+    console.log(`盒马主页获取错误: ` + error);
+  }
+}else if(url.indexOf("querymypage")!=-1){
+  try {
+    const sceneTemplateId = [
+      "906",
+      "907",
+      "198",
+      "286",
+      "431",
+      "185",
+      "230",
+      "978",
+      "709",
+      "432",
+      "403",
+      "350"
+    ];
+    if (obj?.data?.scenes?.length > 0) {
+      let scenes = [];
+      for (let scene of obj.data.scenes) {
+        if (sceneTemplateId.includes(scene.sceneTemplateId)) {
+          scenes.push(scene);
+        }
+      }
+      obj.data.scenes = scenes;
+    }
+  } catch (error) {
+    console.log(`盒马我的页获取错误: ` + error);
+  }
+}else{
+  $done({});
+}
+$done({body:JSON.stringify(obj)});
+
