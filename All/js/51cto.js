40cde6f389b4b1a11d4370f15cd260c17adf1014
Dark Wang <zirawell@gmail.com>
2024-03-16 00:23:42 +0800

    update-20240316

--- All/js/51cto.js
+++ All/js/51cto.js
@@ -20,32 +20,36 @@ hostname = edu.51cto.com
 let body = $response.body;
 const isResponse = typeof $response !== "undefined";
 if(isResponse){
-  let obj = JSON.parse(body);
-  let result = obj.result;
-  if(result.adv_summary){
-    result.adv_summary={};
+  try{
+    let obj = JSON.parse(body);
+    let result = obj.result;
+    if(result.adv_summary){
+      result.adv_summary={};
+    }
+    if(result.hasAd){
+      result.hasAd="0";
+    }
+    if(result.indexAd){
+      result.indexAd={};
+    }
+    if(result.showThird){
+      result.showThird="0";
+    }
+    if(result.status){
+      result.status="0";
+    }
+    if(result.appVersion){
+      result.appVersion="3.5.0";
+    }
+    if(result.ad){
+      result.ad={};
+    }
+    if(result.isExist){
+      result.isExist="0";
+    }
+    body = JSON.stringify(obj);
+  } catch (error) {
+    console.log(`51cto获取错误: ` + error);
   }
-  if(result.hasAd){
-    result.hasAd="0";
-  }
-  if(result.indexAd){
-    result.indexAd={};
-  }
-  if(result.showThird){
-    result.showThird="0";
-  }
-  if(result.status){
-    result.status="0";
-  }
-  if(result.appVersion){
-    result.appVersion="3.5.0";
-  }
-  if(result.ad){
-    result.ad={};
-  }
-  if(result.isExist){
-    result.isExist="0";
-  }
-  body = JSON.stringify(obj);
   $done({ body });
 }
