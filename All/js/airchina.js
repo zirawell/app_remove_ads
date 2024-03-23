c3d902a17d734d5e935832473e086d4433d83d23
Dark Wang <zirawell@gmail.com>
2024-03-23 13:10:22 +0800

    update airchina

---
+++ All/js/airchina.js
@@ -0,0 +1,37 @@
+/********************************
+AirChina Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+^https:\/\/m\.airchina\.com\.cn\/airchina\/gateway\/v\d(\.\d)*\/api\/services url script-response-header https://raw.githubusercontent.com/zirawell/app_remove_ads/main/All/js/airchina.js
+
+Please note that the above rewrite link requires open KOP-XIAO's resource parser
+
+*********************************
+Surge4, Loon and Shadowrocket configuration:
+
+[Script]
+http-request ^https:\/\/m\.airchina\.com\.cn\/airchina\/gateway\/v\d(\.\d)*\/api\/services script-path=https://raw.githubusercontent.com/zirawell/app_remove_ads/main/All/js/airchina.js
+
+[MITM]
+hostname = m.airchina.com.cn
+********************************/
+const url = $request.url;
+const resp = {};
+const header = $request.headers;
+const proc = header["procedure"];
+
+const blockList = [
+  "queryOpenScreenAd",
+  "hasUpgrade",
+  "getLocalHomePage",
+  "getExternalHomePage",
+  "queryImportantList",
+];
+
+if(blockList.includes(proc)){
+  resp.headers = $request.headers;
+  $done(resp);
+}else{
+  $done({});
+}
