5a63f9e7e1b6f2f37fe0c17424945202d237f370
Dark Wang <zirawell@gmail.com>
2024-04-25 23:28:27 +0800

    update flyert

--- All/js/flyert.js
+++ All/js/flyert.js
@@ -22,9 +22,11 @@ const isResponse = typeof $response !== "undefined";
 if(isResponse){
   try {
     let obj = JSON.parse(body);
-    let variables = obj.Variables;
-    if(variables.data && variables.data.adv){
-      variables.data.adv={};
+    if(obj?.Variables){
+      let variables = obj.Variables;
+      if(variables.data && variables.data.adv){
+        variables.data.adv={};
+      }
     }
     body = JSON.stringify(obj);
   } catch (error) {
